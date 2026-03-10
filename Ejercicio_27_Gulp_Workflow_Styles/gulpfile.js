231be5830cfa754e747477af0ce1178455031504
ACount5 <alvaroslkth@gmail.com>
2026-03-10 13:22:49 +0100

    Ejercicios 27-28 añadidos

---
+++ Ejercicio_27_Gulp_Workflow_Styles/gulpfile.js
@@ -0,0 +1,20 @@
+// Tarea: Configura tu gulpfile.js
+// 1. Importa gulp y gulp-sass
+// 2. Define una tarea 'css' para compilar
+// 3. Exporta la tarea default con watch
+
+const { src, dest, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function compilarCSS() {
+    return src('src/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest('dist/css'));
+        
+}
+exports.css = compilarCSS;
+exports.default = function()
+{
+    watch('src/scss/*.scss', compilarCSS)
+};
+
