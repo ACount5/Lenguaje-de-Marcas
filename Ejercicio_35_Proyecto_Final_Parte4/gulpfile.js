16b46de03cb6ce41c1422ac174a31c9199a869bf
ACount5 <alvaroslkth@gmail.com>
2026-03-22 19:24:16 +0100

    modificacion ej.35

--- Ejercicio_35_Proyecto_Final_Parte4/gulpfile.js
+++ Ejercicio_35_Proyecto_Final_Parte4/gulpfile.js
@@ -3,7 +3,7 @@ const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 
 function styles() {
-  return gulp.src('src/scss/main.scss')
+  return gulp.src('src/scss/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS())
     .pipe(gulp.dest('dist/css'));
